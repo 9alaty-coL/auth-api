4472e2e2537a23f7774dfd77f8ac9d50e5356d7c
9alaty-coL <trantanloc@gmail.com>
2022-10-26 19:49:45 +0700

    Add require auth

--- source/routes/auth.js
+++ source/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router  = express.Router()
 const AuthController = require('../controllers/AuthController')
+const isAuth = require('../middlewares/isAuth')
 
 router.post('/login', AuthController.login)
-router.get('/getAll', AuthController.getAllUser)
+router.get('/getAll', isAuth, AuthController.getAllUser)
 router.post('/register', AuthController.register)
 
 module.exports = router
